b2bc174e8d1b6ae744389b2c81e60d256b6b5570
Tinauyateide <tinauyats@gmail.com>
2022-07-26 18:21:35 +0100

    assignment on Module4 lesson2

---
+++ create_db_table.py
@@ -0,0 +1,30 @@
+# import sqlite3
+import sqlite3
+
+#check
+print('sqlite imported successfully')
+
+#create or connect to a database
+conn = sqlite3.connect("SGA_1_3_learners.db")
+
+#check the connection to a database
+print(":connection created successfully!") ; print(type(conn))
+
+# create a cursor object
+c = conn.cursor()
+
+#check
+print(f'cursor created successfully! \n{type(c)}')
+
+# #create a database table:
+c.execute(
+    """
+    CREATE TABLE SGA_1_3_learners(
+        first_name text,
+        last_name text,
+        email_address text,
+        course text
+        )
+        """
+)
+print('students table created successfully')
